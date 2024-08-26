995cb59352ebf4609e259e753fc1688d507cb5eb
Deilons <apolo4748@gmail.com>
2024-08-26 08:42:55 -0500

    Feat: Connettion to the data base

---
+++ Data/AplicationDbContext.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Library_System.Data
+{
+    public class AplicationDbContext : DbContext
+    {
+        public AplicationDbContext(DbContextOptions<AplicationDbContext> options) : base(options)
+        {
+        }
+        
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,9 +1,20 @@
-var builder = WebApplication.CreateBuilder(args);
+using DotNetEnv;
+using Library_System.Data;
+using Microsoft.EntityFrameworkCore;
 
-// Add services to the container.
+var builder = WebApplication.CreateBuilder(args);Env.Load();var dbHost = Environment.GetEnvironmentVariable("DB_HOST");
+var dbPort = Environment.GetEnvironmentVariable("DB_PORT");
+var dbDatabaseName = Environment.GetEnvironmentVariable("DB_DATABASE");
+var dbUser = Environment.GetEnvironmentVariable("DB_USERNAME");
+var dbPassword = Environment.GetEnvironmentVariable("DB_PASSWORD");
+
+var mySqlConnection = $"server={dbHost};port={dbPort};database={dbDatabaseName};uid={dbUser};password={dbPassword}";
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<AplicationDbContext>(options =>
+    options.UseMySql(mySqlConnection,ServerVersion.Parse("8.0.20-mysql")));var app = builder.Build();
+    
 
-var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
